59b9dd829758dfeb72a8258982618c554a7bb975
Nik Everett <neverett@wikimedia.org>
2015-03-23 12:47:18 -0400

    Start blazegraph for integration tests
    We'll use this to test the update tool.
    
    Change-Id: Ib080fe55f36ffb87acbb15301a0b96f09fb72747

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Tools for Querying Wikibase instances with RDF.  Two modules:
 ** Apache Licensed
 * blazegraph - Blazegraph extension to make querying Wikibase instances more efficient
 ** GPLv2 Licensed
+
+Development Notes
+-----------------
+* Eclipse - Works well with m2e.
---
+++ tools/src/test/java/org/wikidata/rdf/tool/BlazegraphCanaryIntegrationTest.java
@@ -0,0 +1,25 @@
+package org.wikidata.rdf.tool;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.junit.Test;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+
+/**
+ * Will fail if there isn't a local Blazegraph instance running on port 9999.
+ * Maven will run Blzegraph on that port during the pre-integration-test phase
+ * but if it fails to start then it won't always notice.
+ */
+public class BlazegraphCanaryIntegrationTest {
+    @Test
+    public void canary() throws IOException {
+        String fetched = Resources.asCharSource(new URL("http://localhost:9999/bigdata/status"), Charsets.UTF_8).read();
+        assertThat(fetched, containsString("queriesPerSecond"));
+    }
+}
