d1e8fabb0b05c98a3a25fcc8adbe549b894bdf0b
Stanislav Malyshev <smalyshev@gmail.com>
2015-04-28 12:44:11 -0700

    T97054: add test for RDF Normalizer
    Change-Id: I9ea491aa71f7592f1dcf2f1ac7b36dbf64ca9d78

---
+++ tools/src/test/java/org/wikidata/query/rdf/tool/rdf/NormalizingRdfHandlerUnitTest.java
@@ -0,0 +1,107 @@
+package org.wikidata.query.rdf.tool.rdf;
+
+import org.junit.Test;
+import org.openrdf.model.Statement;
+import org.openrdf.model.impl.LiteralImpl;
+import org.openrdf.model.impl.URIImpl;
+import org.openrdf.rio.RDFHandler;
+import org.openrdf.rio.RDFHandlerException;
+import org.wikidata.query.rdf.common.uri.Ontology;
+import org.wikidata.query.rdf.common.uri.RDF;
+
+import static org.junit.Assert.assertEquals;
+import static org.wikidata.query.rdf.tool.StatementHelper.statement;
+
+public class NormalizingRdfHandlerUnitTest {
+
+    @Test
+    public void testHandleVersion001() throws RDFHandlerException {
+        testHandleStatement("0.0.1");
+    }
+
+    @Test
+    public void testHandleVersionBeta() throws RDFHandlerException {
+        testHandleStatement("beta");
+    }
+
+    private void testHandleStatement(String suffix) throws RDFHandlerException {
+        StatementChecker checkStatement = new StatementChecker();
+        NormalizingRdfHandler handler = new NormalizingRdfHandler(checkStatement);
+        String testStr = "http://www.wikidata.org/ontology-" + suffix + "#Test";
+
+        Statement s = statement(testStr, RDF.TYPE, Ontology.ITEM);
+        Statement p = statement("Q1", testStr, Ontology.ITEM);
+        Statement o = statement("Q1", RDF.TYPE, new URIImpl(testStr));
+        Statement o2 = statement("Q1", RDF.TYPE, new LiteralImpl(testStr));
+
+        checkStatement.expect("http://www.wikidata.org/ontology#Test", null, null);
+        handler.handleStatement(s);
+        checkStatement.expect(null, "http://www.wikidata.org/ontology#Test", null);
+        handler.handleStatement(p);
+        checkStatement.expect(null, null, "http://www.wikidata.org/ontology#Test");
+        handler.handleStatement(o);
+        checkStatement.expect(null, null, testStr);
+        handler.handleStatement(o2);
+    }
+
+    @Test
+    public void testHandleNamespace() throws RDFHandlerException {
+        StatementChecker checkStatement = new StatementChecker();
+        NormalizingRdfHandler handler = new NormalizingRdfHandler(checkStatement);
+
+        checkStatement.expectURI("http://www.wikidata.org/ontology#Test");
+        handler.handleNamespace("test", "http://www.wikidata.org/ontology-0.0.1#Test");
+        handler.handleNamespace("test", "http://www.wikidata.org/ontology-beta#Test");
+    }
+
+    private final class StatementChecker implements RDFHandler {
+        private String expectSubject;
+        private String expectPredicate;
+        private String expectObject;
+        private String expectURI;
+
+        public void expect(String s, String p, String o) {
+            expectSubject = s;
+            expectPredicate = p;
+            expectObject = o;
+        }
+
+        public void expectURI(String u) {
+            expectURI = u;
+        }
+
+        @Override
+        public void startRDF() throws RDFHandlerException {
+        }
+
+        @Override
+        public void endRDF() throws RDFHandlerException {
+
+        }
+
+        @Override
+        public void handleNamespace(String prefix, String uri) {
+            if (expectURI != null) {
+                assertEquals(expectURI, uri);
+            }
+        }
+
+        @Override
+        public void handleStatement(Statement st) {
+            if (expectSubject != null) {
+                assertEquals(expectSubject, st.getSubject().stringValue());
+            }
+            if (expectPredicate != null) {
+                assertEquals(expectPredicate, st.getPredicate().stringValue());
+            }
+            if (expectObject != null) {
+                assertEquals(expectObject, st.getObject().stringValue());
+            }
+        }
+
+        @Override
+        public void handleComment(String comment) {
+        }
+
+    }
+}
