2eee771ad793ba07fa29b3ebfbbf47220a89d23e
Stanislav Malyshev <smalyshev@gmail.com>
2015-05-13 11:37:02 -0700

    Add options to updater
    Change-Id: If19dc98fb1706142f0307b13e6efa6884f54581c

--- dist/src/script/runUpdate.sh
+++ dist/src/script/runUpdate.sh
@@ -3,13 +3,15 @@
 HOST=http://localhost:9999
 CONTEXT=bigdata
 
-while getopts h:c:n: option
+while getopts h:c:n:l:s option
 do
   case "${option}"
   in
     h) HOST=${OPTARG};;
     c) CONTEXT=${OPTARG};;
     n) NAMESPACE=${OPTARG};;
+    l) LANGS=${OPTARG};;
+    s) SKIPSITE=1;;
   esac
 done
 
@@ -17,10 +19,20 @@ if [ -z "$NAMESPACE" ]
 then
   echo "Usage: $0 -n <namespace> [-h <host>] [-c <context>]"
   exit 1
-else
-  CP=lib/wikidata-query-tools-*-jar-with-dependencies.jar
-  MAIN=org.wikidata.query.rdf.tool.Update
-  SPARQL_URL=$HOST/$CONTEXT/namespace/$NAMESPACE/sparql
-  echo "Updating via $SPARQL_URL"
-  java -cp $CP $MAIN --sparqlUrl $SPARQL_URL
 fi
+
+if [ -z "$LANGS" ]; then
+    ARGS=
+else
+    ARGS="--labelLanguage $LANGS --singleLabel $LANGS"
+fi
+
+if [ ! -z "SKIPSITE" ]; then
+    ARGS="$ARGS --skipSiteLinks"
+fi
+
+CP=lib/wikidata-query-tools-*-jar-with-dependencies.jar
+MAIN=org.wikidata.query.rdf.tool.Update
+SPARQL_URL=$HOST/$CONTEXT/namespace/$NAMESPACE/sparql
+echo "Updating via $SPARQL_URL"
+java -cp $CP $MAIN $ARGS --sparqlUrl $SPARQL_URL
