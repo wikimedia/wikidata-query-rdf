97decc3111321c8cec24c6196accabe5a0c5a790
Stanislav Malyshev <smalyshev@gmail.com>
2016-01-28 09:22:16 -0800

    Revert "Revert "Allow undefined labels for label service""
    This reverts commit f48cc7621469e098dce441c3737f0b692fbad5d5.
    Looks like the problem was elsewhere.
    Use patched version of Blazegraph, see T113374 for broken query
    
    Change-Id: Ib8cc453c2933a6a753f2e07a06b58a42a7aaffda

--- blazegraph/src/main/java/org/wikidata/query/rdf/blazegraph/label/LabelService.java
+++ blazegraph/src/main/java/org/wikidata/query/rdf/blazegraph/label/LabelService.java
@@ -397,6 +397,9 @@ public class LabelService extends AbstractServiceFactory {
         public void resolve(Resolution resolution) {
             resolvedSubject = resolveToIvOrError(resolution.subject(), "subject");
             resolvedLabelType = resolveToIvOrError(resolution.labelType(), "label type");
+            if (resolvedSubject == null || resolvedLabelType == null) {
+                return;
+            }
             // TODO this is one at a time - maybe a batch things?
             fillBestLabels();
             IV label = pickOrBuildBestLabel();
@@ -496,9 +499,10 @@ public class LabelService extends AbstractServiceFactory {
         private IV resolveToIvOrError(IValueExpression expression, String nameOfExpression) {
             Object resolved = expression.get(binding);
             if (resolved == null) {
-                throw new RuntimeException(String.format(Locale.ROOT,
-                        "Refusing to lookup labels for unknown %s (%s). That'd be way way way inefficient.",
-                        nameOfExpression, expression));
+                return null;
+//                throw new RuntimeException(String.format(Locale.ROOT,
+//                        "Refusing to lookup labels for unknown %s (%s). That'd be way way way inefficient.",
+//                        nameOfExpression, expression));
             }
             try {
                 return (IV) resolved;
