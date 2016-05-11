eb9f6f37cfc2cc9ea2da06b9c7870e77c5f03f8e
Artem Kozlov <ak@metaphacts.com>
2016-05-11 09:24:57 +0200

    Add HOST option to runBlazegraph
    Add ability to pass additional blazegraph options.
    
    Change-Id: I5d8659cebeece6a07e53aae824f7e21e1ba6acf1

--- dist/src/script/runBlazegraph.sh
+++ dist/src/script/runBlazegraph.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env bash
 
+HOST=localhost
 CONTEXT=bigdata
 PORT=9999
 DIR=`dirname $0`
 MEMORY=-Xmx8g
+BLAZEGRAPH_OPTS=""
 
 function usage() {
-  echo "Usage: $0 [-d <dir>] [-c <context>] [-p <port>]"
+  echo "Usage: $0 [-h <host>] [-d <dir>] [-c <context>] [-p <port>] [-o <blazegraph options>] "
   exit 1
 }
 
-while getopts c:p:d:? option
+while getopts h:c:p:d:o:? option
 do
   case "${option}"
   in
+    h) HOST=${OPTARG};;
     c) CONTEXT=${OPTARG};;
     p) PORT=${OPTARG};;
     d) DIR=${OPTARG};;
+    o) BLAZEGRAPH_OPTS="${OPTARG}";;
     ?) usage;;
   esac
 done
@@ -33,8 +37,9 @@ java -server -XX:+UseG1GC ${MEMORY} -Dcom.bigdata.rdf.sail.webapp.ConfigParams.p
      -Dcom.bigdata.rdf.sparql.ast.QueryHints.analyticMaxMemoryPerQuery=1073741824 \
      -DASTOptimizerClass=org.wikidata.query.rdf.blazegraph.WikibaseOptimizers \
      -Dorg.wikidata.query.rdf.blazegraph.inline.literal.WKTSerializer.noGlobe=$DEFAULT_GLOBE \
+     ${BLAZEGRAPH_OPTS} \
      -jar jetty-runner*.jar \
-     --host localhost \
+     --host $HOST \
      --port $PORT \
      --path /$CONTEXT \
      blazegraph-service-*.war
