46ae7ca98f5e8d770114236fc8b37da165430d4d
Stanislav Malyshev <smalyshev@gmail.com>
2017-06-16 12:02:34 -0700

    Enable user-supplied options via UPDATER_OPTS
    Change-Id: I5e8fb181ae35f0e9cb6df06813e0d4a9041eee42

--- dist/src/script/runUpdate.sh
+++ dist/src/script/runUpdate.sh
@@ -13,8 +13,9 @@ GC_LOGS="-Xloggc:/var/log/wdqs/wdqs-updater_jvm_gc.%p.log \
          -XX:+UseGCLogFileRotation \
          -XX:NumberOfGCLogFiles=10 \
          -XX:GCLogFileSize=20M"
-
 NAMESPACE=wdq
+UPDATER_OPTS=${UPDATER_OPTS:-""}
+
 while getopts h:c:n:l:t:s option
 do
   case "${option}"
@@ -66,4 +67,4 @@ MAIN=org.wikidata.query.rdf.tool.Update
 SPARQL_URL=$HOST/$CONTEXT/namespace/$NAMESPACE/sparql
 AGENT=-javaagent:lib/jolokia-jvm-1.3.1-agent.jar=port=8778,host=localhost
 echo "Updating via $SPARQL_URL"
-java -cp $CP $MEMORY $GC_LOGS $LOG $TIMEOUT_ARG $AGENT $MAIN $ARGS --sparqlUrl $SPARQL_URL "$@"
+java -cp $CP $MEMORY $GC_LOGS $LOG $TIMEOUT_ARG $AGENT ${UPDATER_OPTS} $MAIN $ARGS --sparqlUrl $SPARQL_URL "$@"
