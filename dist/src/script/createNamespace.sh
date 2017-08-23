6803ed1e1169dcc58c5fbf88e0acd950906fcf48
Stanislav Malyshev <smalyshev@gmail.com>
2017-08-23 15:49:39 -0700

    Add script for creating new namespaces
    Bug: T157676
    Change-Id: Ib6d2ad3740eee3f0524d3bcf387d7f180e59742b

---
+++ dist/src/script/createNamespace.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+# This script can be used to create a new namespace on Blazegraph
+HOST="http://localhost:9999"
+URL="$HOST/bigdata/namespace"
+CATEGORY=$1
+
+PROPS=`mktemp /tmp/wdqs.XXXXXX` || exit 1
+trap "rm -f $PROPS" EXIT
+
+sed -e "s/{NAMESPACE}/$CATEGORY/" < default.properties > $PROPS
+cat $PROPS
+curl -XPOST $URL --data-binary @$PROPS -H 'Content-type: text/plain'
+echo
