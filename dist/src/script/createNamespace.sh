3e525a852049a2b8b27202e681929811f17bf9f2
Stanislav Malyshev <smalyshev@gmail.com>
2017-12-04 11:21:25 -0800

    Shut up curl
    Change-Id: I41d9e41a5c399ea1d6225734341a39ffe562ab1c

--- dist/src/script/createNamespace.sh
+++ dist/src/script/createNamespace.sh
@@ -9,5 +9,5 @@ trap "rm -f $PROPS" EXIT
 
 sed -e "s/{NAMESPACE}/$CATEGORY/" < default.properties > $PROPS
 cat $PROPS
-curl -XPOST $URL --data-binary @$PROPS -H 'Content-type: text/plain'
+curl -s -XPOST $URL --data-binary @$PROPS -H 'Content-type: text/plain'
 echo
