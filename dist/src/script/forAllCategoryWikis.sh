6805648dff4839c9893994d26c7016b51ea7bc16
Stanislav Malyshev <smalyshev@gmail.com>
2018-02-26 12:00:53 -0800

    Update URL and make curl fail if URL is wrong
    Change-Id: Ie55e36ba4a586b82cfd9037b94320cca6ac90dc3
    Bug: T188293

--- dist/src/script/forAllCategoryWikis.sh
+++ dist/src/script/forAllCategoryWikis.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-DUMP_LIST=${DUMP_LIST:-"https://noc.wikimedia.org/conf/categories-rdf.dblist"}
+DUMP_LIST=${DUMP_LIST:-"https://noc.wikimedia.org/conf/dblists/categories-rdf.dblist"}
 DIR=${DIR:-`dirname $0`}
 COMMAND="$1"
 
 if [ -f $DUMP_LIST ]; then
 	fetch="cat"
 else
-	fetch="curl -s -XGET"
+	fetch="curl -s -f -XGET"
 fi
 
 shift
+set -e
 $fetch $DUMP_LIST | while read wiki; do
 	echo "Processing $wiki..."
 	$DIR/$COMMAND $wiki "$@"
