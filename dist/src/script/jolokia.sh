85a8b0f6a94332da308c22db8a691b569e8d7203
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2018-11-19 10:23:17 +0100

    Remove all references to Jolokia.
    We are not using jolokia in production, and it makes no sense to couple
    this project to a specific monitoring tool. Especially since we're not
    testing that it works correctly.
    
    Change-Id: Id2ff9d28c48e0dde0ed7000d498cea3617709ba9

--- dist/src/script/jolokia.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# Use: jolokia.sh {start|stop}
-
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-JAR=$DIR/lib/jolokia-jvm-*-agent.jar
-MAIN=org.wikidata.query.rdf.tool.Update
-java -jar $JAR $1 $MAIN
