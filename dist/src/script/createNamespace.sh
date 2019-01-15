f80eb37008d29f13acb37cb87812f5411a066b8d
Stanislav Malyshev <smalyshev@gmail.com>
2019-01-14 16:08:31 -0800

    Fix script
    Change-Id: Ib3f3ef25f85ea21818dee16a807673464b8cbac8

--- dist/src/script/createNamespace.sh
+++ dist/src/script/createNamespace.sh
@@ -3,10 +3,10 @@ set -e
 
 # This script can be used to create a new namespace on Blazegraph
 ENDPOINT="http://localhost:9999"
-if [ "x$2" != "x" ];
+if [ "x$2" != "x" ]; then
 	ENDPOINT=$2
 fi
-URL="$HOST/bigdata/namespace"
+URL="$ENDPOINT/bigdata/namespace"
 
 CATEGORY=$1
 
