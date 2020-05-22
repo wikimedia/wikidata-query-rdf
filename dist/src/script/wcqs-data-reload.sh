b2ba77bcbf64333875de577858a3c164652b4596
Maryum Styles <mstyles@wikimedia.org>
2020-05-22 16:52:05 -0700

    add script to automate wcqs data load
    Script performs following steps:
    1) Download newest commons dump
    2) Munge the dump
    3) Clear current blazegraph journal
    This removes all namespaces!
    4) Load data into created (by default) wcq namespace.
    This step will fail if default on the host is different
    that wcq.
    
    Bug: T251515
    Change-Id: Idee0b2971f7ba78a020ee9fe1a631472ddee5fec

---
+++ dist/src/script/wcqs-data-reload.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+DUMP_FILENAME=latest-mediainfo.ttl.gz
+DATA_DIR=$(grep 'data_dir:' /etc/wcqs/vars.yaml | awk '{print $2}')
+DEPLOY_DIR=$(grep 'package_dir:' /etc/wcqs/vars.yaml | awk '{print $2}')
+mkdir -p ${DATA_DIR}/munged
+
+trap 'rm -rf ${DATA_DIR}/${DUMP_FILENAME} ${DATA_DIR}/munged' EXIT
+
+echo "Download latest dump"
+curl -o ${DATA_DIR}/${DUMP_FILENAME} \
+https://dumps.wikimedia.org/other/wikibase/commonswiki/${DUMP_FILENAME}
+
+echo "Running munge script"
+${DEPLOY_DIR}/munge.sh \
+-f ${DATA_DIR}/${DUMP_FILENAME} \
+-c 50000 \
+-d ${DATA_DIR}/munged \
+-- \
+--wikibaseHost commons.wikimedia.org \
+--conceptUri http://www.wikidata.org \
+--commonsUri http://commons.wikimedia.org
+
+echo "Stopping Blazegraph"
+sudo systemctl stop wcqs-blazegraph
+
+echo "Deleting blazegraph journal"
+rm -rf ${DATA_DIR}/wcqs.jnl
+
+echo "Starting Blazegraph"
+sudo systemctl start wcqs-blazegraph
+
+echo "Waiting for Blazegraph to restart"
+sleep 30
+
+echo "Loading data"
+${DEPLOY_DIR}/loadData.sh -n wcq -h http://localhost:9999 -d ${DATA_DIR}/munged
