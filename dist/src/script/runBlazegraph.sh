bd61986bd636db0628f15560afc46b59883cdd72
Zbyszko Papierski <zpapierski@wikimedia.org>
2020-05-25 14:29:30 +0200

    Set prefix file through runBlazegraph.sh
    SEfC requires different namespace prefixes to be used
    by Blazegraph, so we need a way to customize those. Prefix
    configuration has been added and a parameter to set the file.
    This allows for prefixes in query service to be consistent
    with Commons dumps. Question remains if user prefer that,
    or prefixes consistent with WDQS.
    
    Bug: T251490
    Change-Id: I3cce0df28cde1120b9ecafdf036bc6dee7c35b85

--- dist/src/script/runBlazegraph.sh
+++ dist/src/script/runBlazegraph.sh
@@ -10,6 +10,9 @@ HOST=${HOST:-"localhost"}
 CONTEXT=bigdata
 PORT=${PORT:-"9999"}
 DIR=${DIR:-`dirname $0`}
+PREFIXES_FILE=$DIR/prefixes.conf
+WIKIBASE_CONCEPT_URI_PARAM=${WIKIBASE_CONCEPT_URI_PARAM:-""}
+COMMONS_CONCEPT_URI_PARAM=${COMMONS_CONCEPT_URI_PARAM:-""}
 HEAP_SIZE=${HEAP_SIZE:-"16g"}
 LOG_CONFIG=${LOG_CONFIG:-""}
 LOG_DIR=${LOG_DIR:-"/var/log/wdqs"}
@@ -47,11 +50,12 @@ DEBUG=-agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=n
 DEBUG=
 
 function usage() {
-  echo "Usage: $0 [-h <host>] [-d <dir>] [-c <context>] [-p <port>] [-o <blazegraph options>] [-f config.properties]"
+  echo "Usage: $0 [-h <host>] [-d <dir>] [-c <context>] [-p <port>] " \
+  "[-o <blazegraph options>] [-f config.properties] [-n prefixes.conf] [-w wikibaseConceptUri] [-m commonsConceptUri]"
   exit 1
 }
 
-while getopts h:c:p:d:o:f:? option
+while getopts h:c:p:d:o:f:n:w:m:? option
 do
   case "${option}"
   in
@@ -61,6 +65,9 @@ do
     d) DIR=${OPTARG};;
     o) BLAZEGRAPH_OPTS="${OPTARG}";;
     f) CONFIG_FILE=${OPTARG};;
+    n) PREFIXES_FILE=${OPTARG};;
+    w) WIKIBASE_CONCEPT_URI_PARAM="-DwikibaseConceptUri=${OPTARG} ";;
+    m) COMMONS_CONCEPT_URI_PARAM="-DcommonsConceptUri=${OPTARG} ";;
     ?) usage;;
   esac
 done
@@ -80,17 +87,19 @@ fi
 echo "Running Blazegraph from `pwd` on :$PORT/$CONTEXT"
 exec java \
      -server -XX:+UseG1GC ${MEMORY} ${DEBUG} ${GC_LOGS} ${LOG_OPTIONS} ${EXTRA_JVM_OPTS} \
-     -Dcom.bigdata.rdf.sail.webapp.ConfigParams.propertyFile=${CONFIG_FILE} \
+     -Dcom.bigdata.rdf.sail.webapp.ConfigParams.propertyFile="${CONFIG_FILE}" \
      -Dorg.eclipse.jetty.server.Request.maxFormContentSize=200000000 \
      -Dcom.bigdata.rdf.sparql.ast.QueryHints.analytic=true \
      -Dcom.bigdata.rdf.sparql.ast.QueryHints.analyticMaxMemoryPerQuery=1073741824 \
      -DASTOptimizerClass=org.wikidata.query.rdf.blazegraph.WikibaseOptimizers \
      -Dorg.wikidata.query.rdf.blazegraph.inline.literal.WKTSerializer.noGlobe=$DEFAULT_GLOBE \
      -Dcom.bigdata.rdf.sail.webapp.client.RemoteRepository.maxRequestURLLength=7168 \
-     -Dcom.bigdata.rdf.sail.sparql.PrefixDeclProcessor.additionalDeclsFile=$DIR/prefixes.conf \
+     -Dcom.bigdata.rdf.sail.sparql.PrefixDeclProcessor.additionalDeclsFile="$PREFIXES_FILE" \
      -Dorg.wikidata.query.rdf.blazegraph.mwapi.MWApiServiceFactory.config=$DIR/mwservices.json \
      -Dcom.bigdata.rdf.sail.webapp.client.HttpClientConfigurator=org.wikidata.query.rdf.blazegraph.ProxiedHttpConnectionFactory \
      -Dhttp.userAgent="${USER_AGENT}" \
+     $WIKIBASE_CONCEPT_URI_PARAM \
+     $COMMONS_CONCEPT_URI_PARAM \
      -Dorg.eclipse.jetty.annotations.AnnotationParser.LEVEL=OFF \
      ${BLAZEGRAPH_OPTS} \
      -jar jetty-runner*.jar \
