7cef478ea1556c9adbf769d9c49ca7dcbf3cb910
Zbyszko Papierski <zpapierski@wikimedia.org>
2020-09-08 15:53:59 +0200

    Category reload shouldn't stop for single category fail
    forAllCategoryWikis.sh will continue after failing on
    a wiki, logging the fact first.
    
    Bug: T261097
    Change-Id: I6bf000cac940ffae3e49e3a81ccb5665fd59a818

--- dist/src/script/forAllCategoryWikis.sh
+++ dist/src/script/forAllCategoryWikis.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e
 
 DUMP_LIST=${DUMP_LIST:-"https://noc.wikimedia.org/conf/dblists/categories-rdf.dblist"}
 DIR=${DIR:-`dirname $0`}
@@ -12,7 +11,16 @@ else
 fi
 
 shift
-$fetch $DUMP_LIST | grep -v '^#' | while read wiki; do
+STATUS=0
+
+for wiki in $($fetch $DUMP_LIST | grep -v '^#');
+do
 	echo "Processing $wiki..."
 	$DIR/$COMMAND $wiki "$@"
+	if [ $? -ne 0 ]; then
+		echo "ERROR:$COMMAND - Failed for $wiki, moving on to the next entry..." 1>&2
+		STATUS=1
+	fi
 done
+
+exit $STATUS
