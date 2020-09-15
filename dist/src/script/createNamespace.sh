5c9042a7dd78b18d20923119815d75fe4b275c32
Zbyszko Papierski <zpapierski@wikimedia.org>
2020-09-15 14:55:35 +0200

    Provide active/standby namespaces for WCQS
    Changes to the reload script that enable
    loading the date alternatively between two namespaces,
    so that we can minimize the downtime to 0.
    
    Bug: T262828
    Change-Id: Ic5ba19bcb736d0a04910d916321f22d15519d49f

--- dist/src/script/createNamespace.sh
+++ dist/src/script/createNamespace.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 set -e
 
 # This script can be used to create a new namespace on Blazegraph
@@ -8,12 +8,17 @@ if [ "x$2" != "x" ]; then
 fi
 URL="$ENDPOINT/bigdata/namespace"
 
-CATEGORY=$1
+NAMESPACE=$1
 
 PROPS=`mktemp /tmp/wdqs.XXXXXX` || exit 1
 trap "rm -f $PROPS" EXIT
 
-sed -e "s/{NAMESPACE}/$CATEGORY/" < default.properties > $PROPS
+if [ "x$3" != "x" ]; then
+  sed -e "s/\.$4\./.$NAMESPACE./" < "$3" > $PROPS
+
+else
+  sed -e "s/{NAMESPACE}/$NAMESPACE/" < default.properties > $PROPS
+fi
 cat $PROPS
 curl --silent --show-error -XPOST $URL --data-binary @$PROPS  -H 'Content-type: text/plain'
 echo
--- dist/src/script/wcqs-data-reload.sh
+++ dist/src/script/wcqs-data-reload.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 set -e
+. /usr/local/bin/cronUtils.sh wcqs
+
+function log() {
+  echo "$(date --iso-8601=seconds) $1"
+}
 
 DUMP_FILENAME=latest-mediainfo.ttl.gz
-DATA_DIR=$(grep 'data_dir:' /etc/wcqs/vars.yaml | awk '{print $2}')
-DEPLOY_DIR=$(grep 'package_dir:' /etc/wcqs/vars.yaml | awk '{print $2}')
 MUNGED_DIR=${DATA_DIR}/munged
 mkdir -p "${MUNGED_DIR}"
+ENDPOINT="http://localhost:9999"
 
 # do not delete the data for now - see https://phabricator.wikimedia.org/T251515#6393397
 # trap 'rm -rf ${DATA_DIR}/${DUMP_FILENAME} ${DATA_DIR}/munged' EXIT
 
-echo "Download latest dump"
+log "Download latest dump"
 curl -o ${DATA_DIR}/${DUMP_FILENAME} \
 https://dumps.wikimedia.org/other/wikibase/commonswiki/${DUMP_FILENAME}
 
-echo "Running munge script"
+log "Running munge script"
 ${DEPLOY_DIR}/munge.sh \
 -f ${DATA_DIR}/${DUMP_FILENAME} \
 -c 50000 \
@@ -25,25 +29,16 @@ ${DEPLOY_DIR}/munge.sh \
 --commonsUri https://commons.wikimedia.org
 
 # added for debugging purposes - see https://phabricator.wikimedia.org/T251515#6393397
-echo "Munged dir - ${MUNGED_DIR}"
+log "Munged dir - ${MUNGED_DIR}"
 
-echo "Switching frontend to data reload page"
-touch /var/lib/nginx/wdqs/data-reload
+NEW_NAMESPACE="wcqs${today}"
+${DEPLOY_DIR}/createNamespace.sh $NEW_NAMESPACE $ENDPOINT /etc/wcqs/RWStore.wcqs.properties wcq || exit 1
 
-echo "Stopping Blazegraph"
-sudo systemctl stop wcqs-blazegraph
+log "Loading data"
+${DEPLOY_DIR}/loadData.sh -n $NEW_NAMESPACE -h $ENDPOINT -d "${MUNGED_DIR}"
 
-echo "Deleting blazegraph journal"
-rm -rf ${DATA_DIR}/wcqs.jnl
+log "Data reload complete, switching active namespace"
+replaceNamespace wcq $NEW_NAMESPACE $ENDPOINT
+log "All done"
 
-echo "Starting Blazegraph"
-sudo systemctl start wcqs-blazegraph
 
-echo "Waiting for Blazegraph to restart"
-sleep 30
-
-echo "Loading data"
-${DEPLOY_DIR}/loadData.sh -n wcq -h http://localhost:9999 -d "${MUNGED_DIR}"
-
-echo "Switching frontend to standard page"
-rm /var/lib/nginx/wdqs/data-reload
