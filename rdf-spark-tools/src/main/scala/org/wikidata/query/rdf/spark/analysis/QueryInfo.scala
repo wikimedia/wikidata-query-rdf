fe0d32137a95c4c17e138dad7cf3a44fc664e7cc
akhatun <akhatun-ctr@wikimedia.org>
2021-08-06 20:17:50 +0600

    Add missing prefixes to prevent SPARQL parsing errors
    Around 11% of SPARQL queries ran into parsing errors
    raised by Jena, due to missing preixes.
    See more stats in T285465.
    This patch adds relevant prefixes. Note that this is not an
    exhaustive list of all prefixes WD and blazegraph handle,
    but covers almost all SPARQL queries.
    
    Bug: T287225
    Change-Id: Icc02674800e6a3de8b9b4fa52966004578e4aaaf

--- rdf-spark-tools/src/main/scala/org/wikidata/query/rdf/spark/analysis/QueryInfo.scala
+++ rdf-spark-tools/src/main/scala/org/wikidata/query/rdf/spark/analysis/QueryInfo.scala
@@ -4,7 +4,7 @@ import org.apache.jena.query.QueryFactory
 import org.apache.jena.sparql.algebra.Algebra
 import org.apache.jena.sparql.algebra.walker.Walker
 import org.wikidata.query.rdf.spark.analysis.visitors.{AnalyzeOpVisitor, TripleInfo}
-
+import org.wikidata.query.rdf.common.uri._
 import scala.collection.mutable
 import scala.util.Try
 
@@ -23,40 +23,35 @@ case class QueryInfo(
 
 object QueryInfo {
 
-  val prefixes = """
-    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-    PREFIX ontolex: <http://www.w3.org/ns/lemon/ontolex#>
-    PREFIX dct: <http://purl.org/dc/terms/>
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    PREFIX owl: <http://www.w3.org/2002/07/owl#>
-    PREFIX wikibase: <http://wikiba.se/ontology#>
-    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-    PREFIX schema: <http://schema.org/>
-    PREFIX cc: <http://creativecommons.org/ns#>
-    PREFIX geo: <http://www.opengis.net/ont/geosparql#>
-    PREFIX prov: <http://www.w3.org/ns/prov#>
-    PREFIX v: <http://www.wikidata.org/value/>
-    PREFIX wd: <http://www.wikidata.org/entity/>
-    PREFIX data: <https://www.wikidata.org/wiki/Special:EntityData/>
-    PREFIX s: <http://www.wikidata.org/entity/statement/>
-    PREFIX ref: <http://www.wikidata.org/reference/>
-    PREFIX wdt: <http://www.wikidata.org/prop/direct/>
-    PREFIX wdtn: <http://www.wikidata.org/prop/direct-normalized/>
-    PREFIX p: <http://www.wikidata.org/prop/>
-    PREFIX ps: <http://www.wikidata.org/prop/statement/>
-    PREFIX psv: <http://www.wikidata.org/prop/statement/value/>
-    PREFIX psn: <http://www.wikidata.org/prop/statement/value-normalized/>
-    PREFIX pq: <http://www.wikidata.org/prop/qualifier/>
-    PREFIX pqv: <http://www.wikidata.org/prop/qualifier/value/>
-    PREFIX pqn: <http://www.wikidata.org/prop/qualifier/value-normalized/>
-    PREFIX pr: <http://www.wikidata.org/prop/reference/>
-    PREFIX prv: <http://www.wikidata.org/prop/reference/value/>
-    PREFIX prn: <http://www.wikidata.org/prop/reference/value-normalized/>
-    PREFIX wdno: <http://www.wikidata.org/prop/novalue/>
-    PREFIX bd: <http://www.bigdata.com/rdf#>
-    PREFIX hint: <http://www.bigdata.com/queryHints#>
-  """
+  def makeURI(prefix: String, namespace: String): String = s"PREFIX $prefix: <$namespace>"
+
+  val prefixDeclarations = List(
+    makeURI("wikibase", Ontology.NAMESPACE),
+    makeURI("skos", SKOS.NAMESPACE),
+    makeURI("ontolex", Ontolex.NAMESPACE),
+    makeURI("mediawiki", Mediawiki.NAMESPACE),
+    makeURI("mwapi", Mediawiki.API),
+    makeURI("geof", GeoSparql.FUNCTION_NAMESPACE),
+    makeURI("geo", GeoSparql.NAMESPACE),
+    makeURI("dct", Dct.NAMESPACE),
+    makeURI("viaf", CommonValues.VIAF),
+    makeURI("owl", OWL.NAMESPACE),
+    makeURI("prov", Provenance.NAMESPACE),
+    makeURI("rdf", RDF.NAMESPACE),
+    makeURI("rdfs", RDFS.NAMESPACE),
+    makeURI("schema", SchemaDotOrg.NAMESPACE),
+    makeURI("gas", "http://www.bigdata.com/rdf/gas#"),
+    makeURI("xsd", "http://www.w3.org/2001/XMLSchema#"),
+    makeURI("cc", "http://creativecommons.org/ns#"),
+    makeURI("bd", "http://www.bigdata.com/rdf#"),
+    makeURI("hint", "http://www.bigdata.com/queryHints#"),
+    makeURI("foaf", "http://xmlns.com/foaf/0.1/"),
+    makeURI("fn", "http://www.w3.org/2005/xpath-functions#"),
+    makeURI("dc", "http://purl.org/dc/elements/1.1/"),
+    UrisSchemeFactory.WIKIDATA.prefixes(new java.lang.StringBuilder()).toString
+  )
+
+  val prefixes = prefixDeclarations.mkString("\n")
 
   def apply(queryString: String): Option[QueryInfo] = Try{
     val queryWithNamespaces = QueryFactory.create(prefixes + queryString)
