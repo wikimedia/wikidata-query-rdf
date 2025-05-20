cd930447aa438d63548b9938d19cdf94d6d918ad
Peter Fischer <pfischer@wikimedia.org>
2025-05-20 11:02:48 +0200

    Update parent POM
    Motivation: Deploy to GitLab package registry instead of archiva. Version 0.3.156 is already imported into wmf-packages, no need to re-release.
    
    Fixes javadoc of `SimpleBooleanExpression` and exemplary show selective spotless formatting.
    
    Bug: T367405
    Change-Id: Ic4ec6f3a8844ad36a0e553e3bbcc3722f09b1a0e

--- blazegraph/src/main/java/org/wikidata/query/rdf/blazegraph/throttling/SimpleBooleanExpression.java
+++ blazegraph/src/main/java/org/wikidata/query/rdf/blazegraph/throttling/SimpleBooleanExpression.java
@@ -15,25 +15,24 @@ import lombok.AllArgsConstructor;
 /**
  * Provides a simple expression language over boolean values.
  *
- * Keys must start with a letter and contain only letters and -
- * Keys will be evaluated via the provided predicate.
+ * <p>Keys must start with a letter and contain only letters and - Keys will be evaluated via the
+ * provided predicate.
  *
- * Supported operators: && (and), ! (not).
+ * <p>Supported operators: &amp;&amp; (and), ! (not).
  *
- * Example expressions:
- *   a && !b
- *   a && b
- *   x-custom-header && !x-other-header
+ * <p>Example expressions: a &amp;&amp; !b a &amp;&amp; b x-custom-header &amp;&amp; !x-other-header
  */
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public final class SimpleBooleanExpression {
-    private static final Predicate<String> VALIDATE_KEY = Pattern.compile("^\\w[-\\w]*$").asPredicate();
+    private static final Predicate<String> VALIDATE_KEY =
+            Pattern.compile("^\\w[-\\w]*$").asPredicate();
     private final List<Function<Predicate<String>, Boolean>> expressions;
 
     public static SimpleBooleanExpression create(String expression) {
-        return new SimpleBooleanExpression(Arrays.stream(expression.split("\\s*&&\\s*"))
-            .map(SimpleBooleanExpression::parsePart)
-            .collect(Collectors.toList()));
+        return new SimpleBooleanExpression(
+                Arrays.stream(expression.split("\\s*&&\\s*"))
+                        .map(SimpleBooleanExpression::parsePart)
+                        .collect(Collectors.toList()));
     }
 
     private static Function<Predicate<String>, Boolean> parsePart(String part) {
