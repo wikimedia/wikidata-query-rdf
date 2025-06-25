963d9e8b90bcbc8e260d5dd5eac9a0185250aba8
Andrew Kostka <andrew.kostka@wikimedia.de>
2025-06-25 13:57:16 +0000

    rdf-spark-tools: populate api_user_agent column
    Bug: T396002
    Change-Id: Ibc403e5eff8516fd6cf9b577b572d3ed611c3d64

--- rdf-spark-tools/src/main/scala/org/wikidata/query/rdf/spark/transform/queries/sparql/QueriesProcessor.scala
+++ rdf-spark-tools/src/main/scala/org/wikidata/query/rdf/spark/transform/queries/sparql/QueriesProcessor.scala
@@ -42,6 +42,7 @@ class QueriesProcessor(tableAndPartitionSpec: String, numPartitions: Int)(implic
           ELSE '5_more_10s'
           END AS query_time_class
         """,
+        "http.request_headers['api-user-agent'] as api_user_agent",
         "http.request_headers['user-agent'] as ua"
         )
       //Accepting 500 as they are likely to be timeout
